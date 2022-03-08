9a6e59eef1df97c4c521747375a3649dc46135f3
Gautham Manohar <gautham.manohar95@gmail.com>
2022-03-08 10:33:14 -0500

    added empty check for addToCart and order

--- Group5_Assignment_1_PHP/addToCart.php
+++ Group5_Assignment_1_PHP/addToCart.php
@@ -2,13 +2,13 @@
 
 include "connect.php";
 
-if(isset($_GET['productId'])){
+if(isset($_GET['productId']) && $_GET['cartItemQty']) && $_GET['email']) && $_GET['price']) && $_GET['totalPrice'])){
 	$productId=$_GET['productId'];
     $cartItemQty=$_GET['cartItemQty'];
     $email=$_GET['email'];
     $price=$_GET['price'];
     $totalPrice=$_GET['totalPrice'];
-    if(!empty($productId)){
+    if(!empty($productId) && !empty($cartItemQty) && !empty($email) && !empty($price) && !empty($totalPrice)){
 		$sql="insert into cart (email, productId, price, totalPrice, cartItemQty) values ('$email','$productId','$price','$totalPrice','$cartItemQty')";
 
 		$fetchdata=mysqli_query($con, $sql);
--- Group5_Assignment_1_PHP/order.php
+++ Group5_Assignment_1_PHP/order.php
@@ -2,7 +2,7 @@
 
 include "connect.php";
 
-if(isset($_GET['orderTotal'])){
+if(isset($_GET['orderTotal']) && $_GET['email'])){
 	$orderTotal=$_GET['orderTotal'];
     $email=$_GET['email'];
     if(!empty($orderTotal)){
